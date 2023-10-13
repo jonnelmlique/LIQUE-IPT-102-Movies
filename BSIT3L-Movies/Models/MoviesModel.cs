1c7ab415514bf76c8568ac1548d8a53f8a12e613
jonnelmlique <jonnelmlique57@gmail.com>
2023-10-14 00:19:05 +0800

    add new string

--- BSIT3L-Movies/Models/MoviesModel.cs
+++ BSIT3L-Movies/Models/MoviesModel.cs
@@ -6,6 +6,7 @@ namespace BSIT3L_Movies.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Details { get; set; }
         public string Rating { get; set; }
         public int ReleaseYear { get; set; }
         public string Genre { get; set; }
